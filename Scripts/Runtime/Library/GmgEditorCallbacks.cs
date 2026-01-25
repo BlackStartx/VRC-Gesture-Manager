e743f2b196257e585ccc66e186236ec122312ee0
BlackStartx <leonardo.lorenzoni99@gmail.com>
2026-01-25 19:37:02 +0100

    [Gesture Manager 3.9.8] Fix for Undo&Unlink issue~

---
+++ Scripts/Runtime/Library/GmgEditorCallbacks.cs
@@ -0,0 +1,35 @@
+﻿namespace BlackStartX.GestureManager.Library
+{
+    public static class GmgEditorCallbacks
+    {
+        public static class Undo
+        {
+            public static bool IsProcessing
+            {
+                get
+                {
+#if UNITY_EDITOR
+                    return UnityEditor.Undo.isProcessing;
+#endif
+                    return false;
+                }
+            }
+
+            public static event UnityEditor.Undo.UndoRedoCallback UndoRedoPerformed
+            {
+                add
+                {
+#if UNITY_EDITOR
+                    UnityEditor.Undo.undoRedoPerformed += value;
+#endif
+                }
+                remove
+                {
+#if UNITY_EDITOR
+                    UnityEditor.Undo.undoRedoPerformed -= value;
+#endif
+                }
+            }
+        }
+    }
+}
